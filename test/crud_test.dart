30cb71d26f96999edaab228744bf9a7830e45b52
Ralf Kistner <ralf@journeyapps.com>
2023-03-02 14:09:29 +0200

    Basic crud recording tests.

---
+++ test/crud_test.dart
@@ -0,0 +1,120 @@
+import 'package:powersync/powersync.dart';
+import 'package:sqlite3/sqlite3.dart' as sqlite;
+import 'package:test/test.dart';
+
+import 'util.dart';
+
+const testId = "2290de4f-0488-4e50-abed-f8e8eb1d0b42";
+
+void main() {
+  setupLogger();
+
+  group('CRUD Tests', () {
+    late PowerSyncDatabase powersync;
+    late String path;
+
+    setUp(() async {
+      path = dbPath();
+      await cleanDb(path: path);
+
+      powersync = await setupPowerSync(path: path);
+    });
+
+    test('INSERT', () async {
+      expect(await powersync.getAll('SELECT * FROM crud'), equals([]));
+      await powersync.execute(
+          'INSERT INTO assets(id, description) VALUES(?, ?)', [testId, 'test']);
+
+      expect(
+          await powersync.getAll('SELECT data FROM crud ORDER BY id'),
+          equals([
+            {
+              'data':
+                  '{"op":"PUT","type":"assets","id":"$testId","data":{"description":"test"}}'
+            }
+          ]));
+
+      await powersync.execute('DELETE FROM crud WHERE 1');
+    });
+
+    test('INSERT OR REPLACE', () async {
+      await powersync.execute(
+          'INSERT INTO assets(id, description) VALUES(?, ?)', [testId, 'test']);
+      await powersync.execute('DELETE FROM crud WHERE 1');
+
+      // Replace
+      await powersync.execute(
+          'INSERT OR REPLACE INTO assets(id, description) VALUES(?, ?)',
+          [testId, 'test2']);
+
+      // This generates another PUT
+      expect(
+          await powersync.getAll('SELECT data FROM crud ORDER BY id'),
+          equals([
+            {
+              'data':
+                  '{"op":"PUT","type":"assets","id":"$testId","data":{"description":"test2"}}'
+            }
+          ]));
+
+      expect(await powersync.get('SELECT count(*) AS count FROM assets'),
+          equals({'count': 1}));
+
+      // Make sure uniqueness is enforced
+      expect(() async {
+        await powersync.execute(
+            'INSERT INTO assets(id, description) VALUES(?, ?)',
+            [testId, 'test3']);
+      },
+          throwsA((e) =>
+              e is sqlite.SqliteException &&
+              e.message.contains('UNIQUE constraint failed')));
+    });
+
+    test('UPDATE', () async {
+      await powersync.execute(
+          'INSERT INTO assets(id, description, make) VALUES(?, ?, ?)',
+          [testId, 'test', 'test']);
+      await powersync.execute('DELETE FROM crud WHERE 1');
+
+      await powersync.execute(
+          'UPDATE assets SET description = ? WHERE id = ?', ['test2', testId]);
+
+      expect(
+          await powersync.getAll('SELECT data FROM crud ORDER BY id'),
+          equals([
+            {
+              'data':
+                  '{"op":"PATCH","type":"assets","id":"$testId","data":{"description":"test2"}}'
+            }
+          ]));
+    });
+
+    test('DELETE', () async {
+      await powersync.execute(
+          'INSERT INTO assets(id, description, make) VALUES(?, ?, ?)',
+          [testId, 'test', 'test']);
+      await powersync.execute('DELETE FROM crud WHERE 1');
+
+      await powersync.execute('DELETE FROM assets WHERE id = ?', [testId]);
+
+      expect(
+          await powersync.getAll('SELECT data FROM crud ORDER BY id'),
+          equals([
+            {'data': '{"op":"DELETE","type":"assets","id":"$testId"}'}
+          ]));
+    });
+
+    test('UPSERT not supported', () async {
+      // Just shows that we cannot currently do this
+      expect(() async {
+        await powersync.execute(
+            'INSERT INTO assets(id, description) VALUES(?, ?) ON CONFLICT DO UPDATE SET description = ?',
+            [testId, 'test2', 'test3']);
+      },
+          throwsA((e) =>
+              e is sqlite.SqliteException &&
+              e.message.contains('cannot UPSERT a view')));
+    });
+  });
+}
