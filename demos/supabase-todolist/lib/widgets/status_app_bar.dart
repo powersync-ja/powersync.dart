5c2b6cbbc2d04f1c075d51b8a396b5bd2a68f339
Ralf Kistner <ralf@journeyapps.com>
2024-01-02 15:23:25 +0200

    Improved status icons on demos.

--- demos/supabase-todolist/lib/widgets/status_app_bar.dart
+++ demos/supabase-todolist/lib/widgets/status_app_bar.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:powersync/powersync.dart';
 import 'package:powersync_flutter_demo/widgets/fts_search_delegate.dart';
@@ -40,16 +41,7 @@ class _StatusAppBarState extends State<StatusAppBar> {
 
   @override
   Widget build(BuildContext context) {
-    const connectedIcon = IconButton(
-      icon: Icon(Icons.wifi),
-      tooltip: 'Connected',
-      onPressed: null,
-    );
-    const disconnectedIcon = IconButton(
-      icon: Icon(Icons.wifi_off),
-      tooltip: 'Not connected',
-      onPressed: null,
-    );
+    final statusIcon = _getStatusIcon(_connectionState);
 
     return AppBar(
       title: Text(widget.title),
@@ -60,8 +52,43 @@ class _StatusAppBarState extends State<StatusAppBar> {
           },
           icon: const Icon(Icons.search),
         ),
-        _connectionState.connected ? connectedIcon : disconnectedIcon
+        statusIcon,
+        // Make some space for the "Debug" banner, so that the status
+        // icon isn't hidden
+        if (kDebugMode) _makeIcon('Debug mode', Icons.developer_mode),
       ],
     );
+  }
+}
+
+Widget _makeIcon(String text, IconData icon) {
+  return Tooltip(
+      message: text,
+      child: SizedBox(width: 40, height: null, child: Icon(icon, size: 24)));
+}
+
+Widget _getStatusIcon(SyncStatus status) {
+  if (status.exception != null) {
+    // The exception message is verbose, could be replaced with something
+    // more user-friendly
+    if (!status.connected) {
+      return _makeIcon(status.exception!.toString(), Icons.cloud_off);
+    } else {
+      return _makeIcon(status.exception!.toString(), Icons.sync_problem);
+    }
+  } else if (status.connecting) {
+    return _makeIcon('Connecting', Icons.cloud_sync_outlined);
+  } else if (!status.connected) {
+    return _makeIcon('Not connected', Icons.cloud_off);
+  } else if (status.uploading && status.downloading) {
+    // The status changes often between downloading, uploading and both,
+    // so we use the same icon for all three
+    return _makeIcon('Uploading and downloading', Icons.cloud_sync_outlined);
+  } else if (status.uploading) {
+    return _makeIcon('Uploading', Icons.cloud_sync_outlined);
+  } else if (status.downloading) {
+    return _makeIcon('Downloading', Icons.cloud_sync_outlined);
+  } else {
+    return _makeIcon('Connected', Icons.cloud_queue);
   }
 }
