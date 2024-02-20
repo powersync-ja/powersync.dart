a3d616b1306e1dd4d13b3e308e7734b56bcf7a3c
benitav <benita@journeyapps.com>
2024-02-20 11:55:43 +0200

    Format comments correctly

--- demos/supabase-todolist/lib/app_config_template.dart
+++ demos/supabase-todolist/lib/app_config_template.dart
@@ -1,7 +1,5 @@
-/**
- * Copy this template: `cp lib/app_config_template.dart lib/app_config.dart`
- * Edit lib/app_config.dart and enter your Supabase and PowerSync project details.
-*/
+// Copy this template: `cp lib/app_config_template.dart lib/app_config.dart`
+// Edit lib/app_config.dart and enter your Supabase and PowerSync project details.
 class AppConfig {
   static const String supabaseUrl = 'https://foo.supabase.co';
   static const String supabaseAnonKey = 'foo';
