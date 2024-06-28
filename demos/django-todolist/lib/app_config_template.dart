d7884da6bef2baeefc4a8ec786d710ef4dc1d9e9
Kobie Botha <kobie@journeyapps.com>
2024-06-28 13:08:48 -0600

    the app :)

---
+++ demos/django-todolist/lib/app_config_template.dart
@@ -0,0 +1,6 @@
+// Copy this template: `cp lib/app_config_template.dart lib/app_config.dart`
+// Edit lib/app_config.dart and enter your Django and PowerSync project details.
+class AppConfig {
+  static const String djangoUrl = 'https://foo.ngrok.app';
+  static const String powersyncUrl = 'https://myprojectid.powersync.journeyapps.com';
+}
