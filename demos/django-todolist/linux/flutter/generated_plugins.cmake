3f5bfce05684f3be7d3252a57e06a2b5a20b8caf
Kobie Botha <kobie@journeyapps.com>
2024-06-28 13:09:07 -0600

    more boilerplate

--- demos/django-todolist/linux/flutter/generated_plugins.cmake
+++ demos/django-todolist/linux/flutter/generated_plugins.cmake
@@ -3,10 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  gtk
   powersync_flutter_libs
   sqlite3_flutter_libs
-  url_launcher_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- demos/django-todolist/windows/flutter/generated_plugins.cmake
+++ demos/django-todolist/windows/flutter/generated_plugins.cmake
@@ -3,10 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  app_links
   powersync_flutter_libs
   sqlite3_flutter_libs
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
