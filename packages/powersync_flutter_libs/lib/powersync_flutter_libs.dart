83a4912a6f566b029ba666cff5861bda0e63cf23
stevensJourney <51082125+stevensJourney@users.noreply.github.com>
2024-07-01 09:13:03 +0200

    chore: fix pana score checks (#110)

--- packages/powersync_flutter_libs/lib/powersync_flutter_libs.dart
+++ packages/powersync_flutter_libs/lib/powersync_flutter_libs.dart
@@ -1,1 +1,4 @@
+/// PowerSync Flutter Libs.
+///
+/// This provides binary files for the [PowerSync SQLite Rust Core](https://github.com/powersync-ja/powersync-sqlite-core)
 library powersync_flutter_libs;
