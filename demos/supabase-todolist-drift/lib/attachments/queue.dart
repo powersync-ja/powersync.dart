73728ce1578c9479f7f0d9a6369ddab0cf3a415a
Mugi Khan <mugikhan@gmail.com>
2024-07-30 16:27:04 +0200

    feat: Initial demo setup

---
+++ demos/supabase-todolist-drift/lib/attachments/queue.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+
+import 'package:powersync/powersync.dart';
+import 'package:powersync_attachments_helper/powersync_attachments_helper.dart';
+import 'package:supabase_todolist_drift/app_config.dart';
+import 'package:supabase_todolist_drift/attachments/remote_storage_adapter.dart';
+
+import 'package:supabase_todolist_drift/models/schema.dart';
+
+/// Global reference to the queue
+late final PhotoAttachmentQueue attachmentQueue;
+final remoteStorage = SupabaseStorageAdapter();
+
+/// Function to handle errors when downloading attachments
+/// Return false if you want to archive the attachment
+Future<bool> onDownloadError(Attachment attachment, Object exception) async {
+  if (exception.toString().contains('Object not found')) {
+    return false;
+  }
+  return true;
+}
+
+class PhotoAttachmentQueue extends AbstractAttachmentQueue {
+  PhotoAttachmentQueue(db, remoteStorage)
+      : super(
+            db: db,
+            remoteStorage: remoteStorage,
+            onDownloadError: onDownloadError);
+
+  @override
+  init() async {
+    if (AppConfig.supabaseStorageBucket.isEmpty) {
+      log.info(
+          'No Supabase bucket configured, skip setting up PhotoAttachmentQueue watches');
+      return;
+    }
+
+    await super.init();
+  }
+
+  @override
+  Future<Attachment> saveFile(String fileId, int size,
+      {mediaType = 'image/jpeg'}) async {
+    String filename = '$fileId.jpg';
+
+    Attachment photoAttachment = Attachment(
+      id: fileId,
+      filename: filename,
+      state: AttachmentState.queuedUpload.index,
+      mediaType: mediaType,
+      localUri: getLocalFilePathSuffix(filename),
+      size: size,
+    );
+
+    return attachmentsService.saveAttachment(photoAttachment);
+  }
+
+  @override
+  Future<Attachment> deleteFile(String fileId) async {
+    String filename = '$fileId.jpg';
+
+    Attachment photoAttachment = Attachment(
+        id: fileId,
+        filename: filename,
+        state: AttachmentState.queuedDelete.index);
+
+    return attachmentsService.saveAttachment(photoAttachment);
+  }
+
+  @override
+  StreamSubscription<void> watchIds({String fileExtension = 'jpg'}) {
+    log.info('Watching photos in $todosTable...');
+    return db.watch('''
+      SELECT photo_id FROM $todosTable
+      WHERE photo_id IS NOT NULL
+    ''').map((results) {
+      return results.map((row) => row['photo_id'] as String).toList();
+    }).listen((ids) async {
+      List<String> idsInQueue = await attachmentsService.getAttachmentIds();
+      List<String> relevantIds =
+          ids.where((element) => !idsInQueue.contains(element)).toList();
+      syncingService.processIds(relevantIds, fileExtension);
+    });
+  }
+}
+
+initializeAttachmentQueue(PowerSyncDatabase db) async {
+  attachmentQueue = PhotoAttachmentQueue(db, remoteStorage);
+  await attachmentQueue.init();
+}
---
+++ demos/supabase-todolist-drift/lib/attachments/remote_storage_adapter.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:powersync_attachments_helper/powersync_attachments_helper.dart';
+import 'package:supabase_todolist_drift/app_config.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:image/image.dart' as img;
+
+class SupabaseStorageAdapter implements AbstractRemoteStorageAdapter {
+  @override
+  Future<void> uploadFile(String filename, File file,
+      {String mediaType = 'text/plain'}) async {
+    _checkSupabaseBucketIsConfigured();
+
+    try {
+      await Supabase.instance.client.storage
+          .from(AppConfig.supabaseStorageBucket)
+          .upload(filename, file,
+              fileOptions: FileOptions(contentType: mediaType));
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  @override
+  Future<Uint8List> downloadFile(String filePath) async {
+    _checkSupabaseBucketIsConfigured();
+    try {
+      Uint8List fileBlob = await Supabase.instance.client.storage
+          .from(AppConfig.supabaseStorageBucket)
+          .download(filePath);
+      final image = img.decodeImage(fileBlob);
+      Uint8List blob = img.JpegEncoder().encode(image!);
+      return blob;
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  @override
+  Future<void> deleteFile(String filename) async {
+    _checkSupabaseBucketIsConfigured();
+
+    try {
+      await Supabase.instance.client.storage
+          .from(AppConfig.supabaseStorageBucket)
+          .remove([filename]);
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  void _checkSupabaseBucketIsConfigured() {
+    if (AppConfig.supabaseStorageBucket.isEmpty) {
+      throw Exception(
+          'Supabase storage bucket is not configured in app_config.dart');
+    }
+  }
+}
