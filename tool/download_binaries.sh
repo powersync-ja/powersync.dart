f1361ef4b09dc7407b74d8569c3f43527a307854
Mugi Khan <mugikhan@gmail.com>
2024-08-19 12:29:09 +0200

    Use melos publish

--- tool/download_binaries.sh
+++ tool/download_binaries.sh
@@ -9,4 +9,4 @@ github="https://github.com/powersync-ja/powersync-sqlite-core/releases/download/
 
 curl "${github}/libpowersync_aarch64.so" -o packages/powersync_flutter_libs/linux/libpowersync_aarch64.so --create-dirs -L -f
 curl "${github}/libpowersync_x64.so" -o packages/powersync_flutter_libs/linux/libpowersync_x64.so --create-dirs -L -f
-curl "${github}/powersync_x64.dll" -o packages/powersync_flutter_libs/windows/powersync.dll --create-dirs -L -f
+curl "${github}/powersync_x64.dll" -o packages/powersync_flutter_libs/windows/powersync_x64.dll --create-dirs -L -f
