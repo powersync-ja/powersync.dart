1236be7bb36cac543d3e0f10ff2e39e6a71193db
Christiaan Landman <chriz.ek@gmail.com>
2024-09-09 08:47:19 +0200

    Fixed widget test.

--- demos/supabase-todolist-optional-sync/test/widget_test.dart
+++ demos/supabase-todolist-optional-sync/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:supabase_todolist_optional_sync/main.dart';
+import 'package:powersync_flutter_supabase_todolist_optional_sync_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
