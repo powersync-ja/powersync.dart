6f9f2b85a60ceb9b9c508fc55ba4a7f254a5f346
Mugi Khan <mugikhan@gmail.com>
2024-09-19 18:42:16 +0200

    Fix imports

--- packages/powersync/lib/src/database/powersync_db_mixin.dart
+++ packages/powersync/lib/src/database/powersync_db_mixin.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 
 import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
-import 'package:powersync/src/connector.dart';
-import 'package:powersync/src/crud.dart';
 import 'package:powersync/sqlite3_common.dart';
 import 'package:powersync/sqlite_async.dart';
 import 'package:powersync/src/abort_controller.dart';
+import 'package:powersync/src/connector.dart';
+import 'package:powersync/src/crud.dart';
 import 'package:powersync/src/powersync_update_notification.dart';
 import 'package:powersync/src/schema.dart';
 import 'package:powersync/src/schema_logic.dart';
