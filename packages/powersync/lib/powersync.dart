7a88bfd49adf054ed1e56b78179ec5a187d015ac
Mugi Khan <mugikhan@gmail.com>
2024-10-17 10:09:13 +0200

    Cleanup powersync export

--- packages/powersync/lib/powersync.dart
+++ packages/powersync/lib/powersync.dart
@@ -3,13 +3,4 @@
 /// Use [PowerSyncDatabase] to open a database.
 library;
 
-export 'package:powersync_core/src/connector.dart';
-export 'package:powersync_core/src/crud.dart';
-export 'package:powersync_core/src/database/powersync_database.dart';
-export 'package:powersync_core/src/exceptions.dart';
-export 'package:powersync_core/src/log.dart';
-export 'package:powersync_core/src/open_factory.dart';
-export 'package:powersync_core/src/powersync_database.dart';
-export 'package:powersync_core/src/schema.dart';
-export 'package:powersync_core/src/sync_status.dart';
-export 'package:powersync_core/src/uuid.dart';
+export 'package:powersync_core/powersync_core.dart';
