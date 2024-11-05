cbc930493231c4d25418bd88ef41c734e6d2e34e
stevensJourney <steven@journeyapps.com>
2024-11-05 10:40:54 +0200

    chore: add melos version commit prompt

--- tool/update_version.dart
+++ tool/update_version.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+
+import 'package:interact/interact.dart';
 import 'package:yaml/yaml.dart';
 
 void main() {
@@ -9,4 +11,20 @@ void main() {
 
   final versionFile = File('packages/powersync/lib/src/version.dart');
   versionFile.writeAsStringSync("const String libraryVersion = '$version';\n");
+
+  // Melos works best when conventional commits are used.
+  // We don't strictly follow this pattern which produces unexpected
+  // Changelogs that sometimes require manual editing.
+  // This script runs before Melos commits the changes.
+  // We can allow a user to edit these Changelogs before commit.
+  bool confirmed = Confirm(
+          prompt:
+              'Melos changelogs should be staged for commit. These changes will be committed and tagged. Feel free to edit them before proceeding. Would you like to proceed now?',
+          defaultValue: true)
+      .interact();
+
+  if (confirmed == false) {
+    // Exit with a non-zero code. This will stop the `melos version` process.
+    exit(1);
+  }
 }
