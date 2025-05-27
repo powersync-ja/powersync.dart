f72389c83343cb0c6dddaf19055e9b734df0d32d
Simon Binder <simon@journeyapps.com>
2025-05-27 23:33:04 +0200

    Also test web

---
+++ packages/powersync_sqlcipher/example/test/worker_server.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+import 'package:stream_channel/stream_channel.dart';
+
+const _corsHeaders = {'Access-Control-Allow-Origin': '*'};
+
+Middleware cors() {
+  Response? handleOptionsRequest(Request request) {
+    if (request.method == 'OPTIONS') {
+      return Response.ok(null, headers: _corsHeaders);
+    } else {
+      // Returning null will run the regular request handler
+      return null;
+    }
+  }
+
+  Response addCorsHeaders(Response response) {
+    return response.change(headers: _corsHeaders);
+  }
+
+  return createMiddleware(
+    requestHandler: handleOptionsRequest,
+    responseHandler: addCorsHeaders,
+  );
+}
+
+Future<void> hybridMain(StreamChannel<Object?> channel) async {
+  final sqliteOutputPath = p.join('web', 'sqlite3mc.wasm');
+
+  if (!(await File(sqliteOutputPath).exists())) {
+    throw AssertionError(
+      'sqlite3mc.wasm file should be present in the ./web folder',
+    );
+  }
+
+  final server = await HttpServer.bind('localhost', 0);
+
+  final handler = const Pipeline()
+      .addMiddleware(cors())
+      .addHandler(createStaticHandler('web'));
+  io.serveRequests(server, handler);
+
+  channel.sink.add(server.port);
+  await channel.stream.listen(null).asFuture<void>().then<void>((_) async {
+    print('closing server');
+    await server.close();
+  });
+}
