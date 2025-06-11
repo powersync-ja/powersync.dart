735d9333305d164e82f882ac28ef2641c7f27483
Simon Binder <simon@journeyapps.com>
2025-06-11 13:45:00 +0200

    Update powersync-sqlite-core to 0.4.0

--- scripts/download_core_binary_demos.dart
+++ scripts/download_core_binary_demos.dart
@@ -3,7 +3,7 @@
 import 'dart:io';
 
 final coreUrl =
-    'https://github.com/powersync-ja/powersync-sqlite-core/releases/download/v0.3.14';
+    'https://github.com/powersync-ja/powersync-sqlite-core/releases/download/v0.4.0';
 
 void main() async {
   final powersyncLibsLinuxPath = "packages/powersync_flutter_libs/linux";
