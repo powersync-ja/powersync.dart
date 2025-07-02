e0cb53b1ad9d267476c7145b15acf20b60d6d885
Simon Binder <simon@journeyapps.com>
2025-07-02 14:46:37 -0600

    Convert supabase-todolist-drift

--- demos/supabase-todolist-drift/android/app/src/main/kotlin/com/example/supabase_todolist_drift/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.supabase_todolist_drift
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ demos/supabase-todolist-drift/android/app/src/main/kotlin/com/powersync/demo/flutter/supabase_todolist_drift/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.powersync.demo.flutter.supabase_todolist_drift
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
