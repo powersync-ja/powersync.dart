5f3bac75654b2ceff3d94cd011828f06b82129b4
Simon Binder <simon@journeyapps.com>
2026-03-26 13:47:15 +0100

    Delete powersync_flutter_libs

--- packages/powersync_flutter_libs/windows/powersync_flutter_libs_plugin.cpp
+++
@@ -1,6 +0,0 @@
-#include "include/powersync_flutter_libs/powersync_flutter_libs_plugin.h"
-
-void PowersyncFlutterLibsPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
-
-}
